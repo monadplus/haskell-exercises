d088b8b2a3dafa93cc971dcd7294a597f0369eb9
Arnau Abella <arnauabella@gmail.com>
2019-11-22 09:05:27 +0100

    ConstraintKinds exercisees done

--- 07-ConstraintKinds/src/Exercises.hs
+++ 07-ConstraintKinds/src/Exercises.hs
@@ -6,16 +6,20 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-} -- Linee 75
+{-# LANGUAGE TypeApplications #-}
 module Exercises where -- ^ This is starting to look impressive, right?
 
 import Data.Kind (Constraint, Type)
+import Data.Monoid ((<>), Sum(..))
+import Data.Proxy
 
 -- | Just a quick one today - there really isn't much to cover when we talk
 -- about ConstraintKinds, as it's hopefully quite an intuitive extension: we're
 -- just extending the set of things that we can use as constraints to include
 -- type parameters!
 
-
+-- ConstraintKinds: https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/glasgow_exts.html#extension-ConstraintKinds
 
 
 
@@ -34,17 +38,34 @@ data List a = Nil | Cons a (List a)
 -- constraints can the @Nil@ case satisfy?
 
 data ConstrainedList (c :: Type -> Constraint) where
-  -- IMPLEMENT ME
+  CNil :: ConstrainedList c -- ConstrainedList (const ())
+  CCons :: c a => a -> ConstrainedList c -> ConstrainedList c
 
 -- | b. Using what we know about RankNTypes, write a function to fold a
 -- constrained list. Note that we'll need a folding function that works /for
 -- all/ types who implement some constraint @c@. Wink wink, nudge nudge.
 
--- foldConstrainedList :: ???
+foldConstrainedList
+  :: Monoid m
+  => (forall x. c x => x -> m)
+  -> ConstrainedList c
+  -> m
+foldConstrainedList _ CNil = mempty
+foldConstrainedList f (CCons x xs) = f x <> foldConstrainedList f xs -- brings c x into scope
+
+foldlConstrainedList
+  :: forall c r. (forall x. c x => r -> x -> r)
+  -> r
+  -> ConstrainedList c
+  -> r
+foldlConstrainedList f r = go r
+  where
+    go r CNil = r
+    go r (CCons x xs) = go (f r x) xs
 
 -- | Often, I'll want to constrain a list by /multiple/ things. The problem is
 -- that I can't directly write multiple constraints into my type, because the
--- kind of @(Eq, Ord)@ isn't @Type -> Constraint@ - it's actually a kind error!
+-- kind of @(Eq, Ord)@ isn't @Type -> Constraint@ - it's actually a kind error!   :kind! (Eq, Int)
 
 -- | There is hope, however: a neat trick we can play is to define a new class,
 -- whose super classes are all the constraints we require. We can then write an
@@ -54,12 +75,24 @@ data ConstrainedList (c :: Type -> Constraint) where
 -- combines `Monoid a` and `Show a`. What other extension did you need to
 -- enable? Why?
 
--- class ??? => Constraints a
--- instance ??? => Constraints a
+class (Show a , Monoid a) => ShowMonoid a
+instance (Show a, Monoid a) => ShowMonoid a
+-- UndecidableInstances is required because nothing is getting "smaller" - when
+-- GHC encounters this constraint, it ends up with more things to solve! This
+-- means that GHC's (limited) termination checker can't be convinced that we'll
+-- ever terminate.
+
 
 -- | What can we now do with this constrained list that we couldn't before?
 -- There are two opportunities that should stand out!
 
+-- We can show everything in it, and we can fill it with 'mempty'.
+-- We can't write a monoid instance, because we have no way of telling that two
+-- constrained lists contain the /same/ monoids. :(
+
+-- >>> xs = CCons (Sum 1) (CCons (Sum 2) CNil) :: ConstrainedList ShowMonoid
+-- >>> foldConstrainedList show xs
+-- >>> foldConstrainedList (const mempty) xs
 
 
 
@@ -78,14 +111,38 @@ data HList (xs :: [Type]) where
 -- the list implemented a given constraint... if only we had a type family for
 -- this...
 
+type family CAll (c :: Type -> Constraint) (xs :: [Type]) :: Constraint where
+  CAll _       '[] = ()
+  CAll c (x ': xs) = (c x, CAll c xs)
+
+data TCProxy (c :: Type -> Constraint) where
+  TCProxy :: TCProxy c
+
 -- | a. Write this fold function. I won't give any hints to the definition, but
 -- we will probably need to call it like this:
 
--- test :: ??? => HList xs -> String
--- test = fold (TCProxy :: TCProxy Show) show
+fold
+  :: (Monoid m, CAll c xs)
+  => TCProxy c
+  -> (forall x. c x => x -> m)
+  -> HList xs
+  -> m
+fold     _ _         HNil = mempty
+fold proxy f (HCons x xs) = f x <> (fold proxy f xs)
+
+test :: CAll Show xs => HList xs -> String
+test = fold (TCProxy @Show) show
+-- >>> xs = HCons (1 :: Int) (HCons True (HCons "hi" HNil))
+-- >>> test xs
 
 -- | b. Why do we need the proxy to point out which constraint we're working
 -- with?  What does GHC not like if we remove it?
+
+--Could not deduce: CAll c0 xs from the context: (Monoid m, CAll c xs)
+--bound by the type signature for: fold :: (Monoid m, CAll c xs) => (forall x. c x => x -> m) -> HList xs -> m
+--at src/Exercises.hs:(125,6)-(128,6)
+
+-- The type is ambiguous for GHC.
 
 -- | We typically define foldMap like this:
 
@@ -101,3 +158,25 @@ f :: a ~ b => a -> b
 f = id
 
 -- | Write @foldMap@ for @HList@!
+{--
+foldMapH
+  :: (Monoid m)
+  => (a -> m)
+  -> HList xs
+  -> m
+foldMapH _ HNil = mempty
+foldMapH f (HCons a as) = f a <> (foldMapH f as)
+
+Could not deduce: x ~ a
+      from the context: xs ~ (x : xs1)<Paste>
+-}
+foldMapH
+  :: (Monoid m, CAll ((~) a) xs)
+  => (a -> m)
+  -> HList xs
+  -> m
+foldMapH _ HNil = mempty
+foldMapH f (HCons a as) = f a <> (foldMapH f as)
+-- >>> xs = HCons (1 :: Int) (HCons (2 :: Int) (HCons (5 :: Int) HNil))
+-- >>> foldMapH Sum xs
+-- Sum 8
